18287368dc3e27e409e138c78987cf041e6edac3
njitvjk <90334123+njitvjk@users.noreply.github.com>
2023-04-05 11:45:59 -0400

    my_first_spring_boot_tranining

---
+++ demo/src/main/java/com/example/demo/dao/PersonDataAccessService.java
@@ -0,0 +1,24 @@
+package com.example.demo.dao;
+
+import com.example.demo.dao.PersonDao;
+import com.example.demo.model.Person;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Repository("fakeDao")
+public class PersonDataAccessService implements PersonDao {
+    private static List<Person> DB =new ArrayList<>();
+    @Override
+    public int insertPerson(UUID id, Person person) {
+        DB.add(new Person(id,person.getName()));
+        return 1;
+    }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        return DB;
+    }
+}
